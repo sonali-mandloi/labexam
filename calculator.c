106e1cf7aaa10a397d56be1383337dfd75b3ffa7
sonali-mandloi <engsonalimandloi@gmail.com>
2019-12-05 06:39:22 -0800

    calculator commited

---
+++ calculator.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int a=40,b=20;
+printf("***********calculator****************");
+printf("addition is %d \n",a+b);
+printf("substraction is %d \n",a-b);
+printf("multiplication is %d \n",a*b);
+printf("division is %d \n",a/b);
+
+return 0;
+}
