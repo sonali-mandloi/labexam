a960914a92a4d641561e1d17c5ea33b70e2d2396
sonali-mandloi <engsonalimandloi@gmail.com>
2019-12-05 06:45:44 -0800

    cal commited

--- calculator.c
+++ calculator.c
@@ -2,7 +2,7 @@
 int main()
 {
 int a=40,b=20;
-printf("***********calculator****************");
+printf("***********calculator****************\n");
 printf("addition is %d \n",a+b);
 printf("substraction is %d \n",a-b);
 printf("multiplication is %d \n",a*b);
